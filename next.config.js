f09861db2419e1a395118462ed1add595ae5e94e
joel-chad <joelchadambuka@gmail.com>
2023-10-10 03:54:26 +0200

    push

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'images.pexels.com',
+            port: '',
+            pathname: '/photos/**'
+        }
+    ]
+}
 }
 
 module.exports = nextConfig
